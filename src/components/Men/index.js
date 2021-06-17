61d0dd2fea5938c67a7837399c0aeb96a08bbded
Bindacharya Jahagirdar <bindacharya.logmein.com>
2021-06-17 17:46:15 +0530

    Initial Commit

---
+++ src/components/Men/index.js
@@ -0,0 +1,17 @@
+//Dependencies
+import React from 'react';
+//Internals
+import MenItems from './MenItems';
+import './index.css';
+
+const MensProducts = () => (
+  <div className="mens-products">
+    <div className="mens-title">
+      <h1>Men's Items</h1>
+    </div>
+    <MenItems />
+  </div>
+);
+
+
+export default MensProducts;
